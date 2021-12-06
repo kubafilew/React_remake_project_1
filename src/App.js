6a028eaf0e417f151ff954d3e8493aca2b7dd581
Kuba Filew <kubafilew@protonmail.com>
2021-12-06 19:19:15 +0100

    update footer , app

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Nav from "./components/Nav";
 import Section1 from "./components/Section1";
 import Section2 from "./components/Section2";
 import Section3 from "./components/Section3";
+import Footer from "./components/Footer";
 
 export const App = () => {
   return (
