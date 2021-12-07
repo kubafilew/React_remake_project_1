cdb2ab1a2554da2e71cead9a0b9ae39e6cafd0c1
Kuba Filew <kubafilew@protonmail.com>
2021-12-07 18:17:02 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 
 import "./App.css";
+
 import Nav from "./components/Nav";
 import Section1 from "./components/Section1";
 import Section2 from "./components/Section2";
 import Section3 from "./components/Section3";
-import Footer from "./components/Footer";
 
 export const App = () => {
   return (
